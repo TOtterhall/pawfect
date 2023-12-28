dd434ddd8e3d287353155b3bad782a0350a3c867
Therese <T.Otterhall@gmail.com>
2023-12-28 18:53:42 +0100

    add frontendsetup

---
+++ client/my-app/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["donlorino.com", "theoddpup.com", "bjaf.se"], // Lägg till domänen här
+  },
+};
+
+module.exports = nextConfig;
