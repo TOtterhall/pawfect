cea08d2706c7f5f25e5e9623986f0c991a0a0211
Therese <T.Otterhall@gmail.com>
2024-01-02 15:50:05 +0100

    add function getSpecificProduct

--- server/src/resources/product/product.controller.js
+++ server/src/resources/product/product.controller.js
@@ -1,14 +1,6 @@
 const { ProductModel } = require("./product.model");
 
-async function getAllProducts(req, res) {
-  try {
-    const products = await ProductModel.find({ deleted: false });
-    res.status(200).json(products);
-  } catch (err) {
-    next(err);
-  }
-}
-
+//FUNCTION- ADD PRODUCT
 async function addProduct(req, res, next) {
   try {
     const product = new ProductModel(req.body);
@@ -19,7 +11,34 @@ async function addProduct(req, res, next) {
   }
 }
 
+//FUNCTION- GET ALL PRODUCTS
+async function getAllProducts(req, res) {
+  try {
+    const products = await ProductModel.find({ deleted: false });
+    res.status(200).json(products);
+  } catch (err) {
+    next(err);
+  }
+}
+
+//FUNCTION- GET SPECIFIK PRODUCT(ID)
+async function getSpecificProduct(req, res) {
+  try {
+    const productId = req.params._id;
+    const product = await ProductModel.findById(productId);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: "Produkten kunde inte hittas... du får nosa vidare" });
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    next(err);
+  }
+}
+
 module.exports = {
-  getAllProducts,
   addProduct,
+  getAllProducts,
+  getSpecificProduct,
 };
--- server/src/resources/product/product.router.js
+++ server/src/resources/product/product.router.js
@@ -1,11 +1,16 @@
 const { Router } = require("express");
 
-const { addProduct, getAllProducts } = require("./product.controller");
+const {
+  addProduct,
+  getAllProducts,
+  getSpecificProduct,
+} = require("./product.controller");
 
 const { ProductModel } = require("./product.model");
 
 const productRouter = Router();
-productRouter.get("/products", getAllProducts);
 productRouter.post("/products", addProduct);
+productRouter.get("/products", getAllProducts);
+productRouter.get("/products/:_id", getSpecificProduct);
 
 module.exports = { productRouter };
