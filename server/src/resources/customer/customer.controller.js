65c5a132c3297a7b5be60af6aa0f6c84f9e86762
Therese <T.Otterhall@gmail.com>
2024-01-03 14:27:42 +0100

    :hammer: add if existing customer to register function

--- server/src/resources/customer/customer.controller.js
+++ server/src/resources/customer/customer.controller.js
@@ -1,11 +1,20 @@
 const { CustomerModel } = require("./customer.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // ADD CUSTOMER
 async function registerCustomer(req, res, next) {
   try {
-    //UTÖKA OM ANVÄDAREN REDAN FINNS_SKA INTE KUNNA REGISTRERA SIG DÅ
     const { email, password } = req.body;
+    //UTÖKA OM ANVÄDAREN REDAN FINNS_SKA INTE KUNNA REGISTRERA SIG DÅ
+
+    const existingCustomer = await CustomerModel.findOne({ email: email });
+    if (existingCustomer) {
+      return res.status(409).json({
+        message:
+          "Denna email är redan registrerad, du får tassa vidare och testa igen...",
+      });
+    }
     //HASHES PASSWORD
     const hashedPassword = await bcrypt.hash(password, 10);
     const customer = new CustomerModel({
@@ -13,11 +22,22 @@ async function registerCustomer(req, res, next) {
       password: hashedPassword,
     });
     await customer.save();
-    res.status(201).json(customer);
+    //TOKEN INNAN ANVÄNDAREN SPARAS
+    const token = jwt.sign(
+      { customerId: customer._id },
+      "my2oTalS3c3tk3Y4you",
+      { expiresIn: "4h" }
+    );
+
+    res.status(201).json({ customer, token });
   } catch (err) {
     next(err);
   }
 }
+
+//FUNCTION LOGIN
+//FUNCTION LOOUT
+//FUNCTION AUTH
 
 // // ADD CUSTOMER
 // async function registerCustomer(req, res, next) {
