c3d072d2e25ce683237b10569f7edbaef353d2b3
Therese <T.Otterhall@gmail.com>
2024-01-03 14:02:13 +0100

    :card_file_box: add bcrypt and jwt hashedpassword

--- server/src/resources/customer/customer.controller.js
+++ server/src/resources/customer/customer.controller.js
@@ -1,14 +1,33 @@
 const { CustomerModel } = require("./customer.model");
+const bcrypt = require("bcrypt");
 
 // ADD CUSTOMER
 async function registerCustomer(req, res, next) {
   try {
-    const customer = new CustomerModel(req.body);
+    //UTÖKA OM ANVÄDAREN REDAN FINNS_SKA INTE KUNNA REGISTRERA SIG DÅ
+    const { email, password } = req.body;
+    //HASHES PASSWORD
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const customer = new CustomerModel({
+      email: email,
+      password: hashedPassword,
+    });
     await customer.save();
     res.status(201).json(customer);
   } catch (err) {
     next(err);
   }
 }
+
+// // ADD CUSTOMER
+// async function registerCustomer(req, res, next) {
+//   try {
+//     const customer = new CustomerModel(req.body);
+//     await customer.save();
+//     res.status(201).json(customer);
+//   } catch (err) {
+//     next(err);
+//   }
+// }
 
 module.exports = { registerCustomer };
