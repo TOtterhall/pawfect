0e215a39ea859fef4bfb4e153e42a9ae61c8c360
Therese <T.Otterhall@gmail.com>
2024-01-09 11:47:29 +0100

    :hammer:  added navbar

--- client/my-app/src/app/components/Header/Header.jsx
+++ client/my-app/src/app/components/Header/Header.jsx
@@ -6,13 +6,8 @@ import SearchForm from "./SearchForm/SearchForm";
 export default function Header() {
   return (
     <div>
-      <h1>MyHeader</h1>
       <p>Min navbar nedanför</p>
       <Navbar />
-      <SearchForm />
-      <h3>Logo</h3>
-      <h3>Login</h3>
-      <h3>Cart</h3>
     </div>
   );
 }
--- client/my-app/src/app/components/Header/Navbar/Navbar.jsx
+++ client/my-app/src/app/components/Header/Navbar/Navbar.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Menu from "../Menu/Menu";
+import SearchForm from "../SearchForm/SearchForm";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Menu />
+      <h1>PAWFECT</h1>
+      <SearchForm />
+      <h3>login</h3>
+      <h3>cart</h3>
+    </div>
+  );
 }
