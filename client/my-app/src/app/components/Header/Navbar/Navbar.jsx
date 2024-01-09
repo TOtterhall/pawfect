db5f45284823d0d858ce32b08469e1075ca2ad6f
Therese <T.Otterhall@gmail.com>
2024-01-09 12:02:13 +0100

    :hammer: added testLogo and navbar

--- client/my-app/src/app/components/Header/Navbar/Navbar.jsx
+++ client/my-app/src/app/components/Header/Navbar/Navbar.jsx
@@ -1,15 +1,50 @@
 import React from "react";
 import Menu from "../Menu/Menu";
 import SearchForm from "../SearchForm/SearchForm";
-
+import Logo from "../Logo/Logo";
 export default function Navbar() {
   return (
     <div>
-      <Menu />
-      <h1>PAWFECT</h1>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand" href="#">
+            Home
+          </a>
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">
+                Disabled
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <SearchForm />
-      <h3>login</h3>
-      <h3>cart</h3>
+      <div>
+        <Logo />
+      </div>
+      <Menu />
     </div>
   );
 }
