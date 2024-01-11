fe9fb6780ea5bd0654e397ff1dc8cab2e03ee212
Therese <T.Otterhall@gmail.com>
2024-01-11 10:12:41 +0100

    :hammer: pulled from shop-client-setup now the routing works

--- client/my-app/src/app/store/page.jsx
+++ client/my-app/src/app/store/page.jsx
@@ -1,10 +1,18 @@
+"use client";
+import React from "react";
 import Shop from "../components/Shop/Shop";
+import ProductProvider from "../../Context/productContext/productContext";
+import CustomerProvider from "../../Context/customerContext/customerContext";
 
 export default function Store() {
   return (
-    <div>
-      <div>Min butik</div>
-      <Shop />
-    </div>
+    <ProductProvider>
+      <CustomerProvider>
+        <main>
+          <div>Min butik</div>
+          <Shop />
+        </main>
+      </CustomerProvider>
+    </ProductProvider>
   );
 }
