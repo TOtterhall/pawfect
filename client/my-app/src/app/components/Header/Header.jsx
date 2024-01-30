5619175e12d7f9650fee68ef8cb6f9f075432b9a
Therese <T.Otterhall@gmail.com>
2024-01-30 10:12:36 +0100

    :lipstick: header and navbar styles

--- client/my-app/src/app/components/Header/Header.jsx
+++ client/my-app/src/app/components/Header/Header.jsx
@@ -1,11 +1,54 @@
 "use client";
 import React from "react";
 import Navbar from "./Navbar/Navbar";
-
+import "./Header.css";
 export default function Header() {
   return (
-    <>
+    <header className="header">
       <Navbar />
-    </>
+    </header>
   );
 }
+// "use client";
+// import React from "react";
+// import "./Header.css";
+// export default function Header() {
+//   return (
+//     <header className="header">
+//       <div className="headercontent container-fluid d-flex justify-content-between align-items-center fixed-top">
+//         <div className="d-flex">
+//           <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//             <span>Test Menu</span>
+//           </nav>
+
+//           <span>Test Search</span>
+//         </div>
+
+//         <div className="text-center text-center flex-grow-1">
+//           <a className="nav-link" href="/">
+//             <h1>PAWFECT</h1>
+//           </a>
+//         </div>
+
+//         <div className="d-flex">
+//           <div>
+//             <button className="btn btn-outline-success my-2 my-sm-0">
+//               Test Login
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+// "use client";
+// import React from "react";
+// import Navbar from "./Navbar/Navbar";
+
+// export default function Header() {
+//   return (
+//     <header>
+//       <Navbar />
+//     </header>
+//   );
+// }
--- client/my-app/src/app/components/Header/Navbar/Navbar.jsx
+++ client/my-app/src/app/components/Header/Navbar/Navbar.jsx
@@ -1,40 +1,73 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import SearchForm from "../SearchForm/SearchForm";
 import Logo from "../Logo/Logo";
 import Login from "../Login/Login";
 import Cart from "../Cart/Cart";
 import Menu from "../Menu/Menu";
-export default function Navbar() {
+export default function Header() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div>
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li>
-              <div>
-                <Menu />
-              </div>
-            </li>
-          </ul>
+    <header className="header">
+      <div className="headercontent container-fluid d-flex justify-content-between align-items-center fixed-top">
+        <div className="d-flex">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <span>
+              <Menu />
+            </span>
+          </nav>
+
+          <span>
+            <SearchForm />
+          </span>
         </div>
 
-        <div>
-          <SearchForm />
-        </div>
-        <div>
+        <div className="text-center text-center">
           <Link className="nav-link" href="/">
             <Logo />
           </Link>
         </div>
-        <div>
-          <Login />
+
+        <div className="d-flex">
+          <div>
+            <Login />
+          </div>
+          <div>
+            <Cart />
+          </div>
         </div>
-        <div>
-          <Cart />
-        </div>
-      </nav>
-    </div>
+      </div>
+    </header>
   );
 }
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import Link from "next/link";
+// import SearchForm from "../SearchForm/SearchForm";
+// import Logo from "../Logo/Logo";
+// import Login from "../Login/Login";
+// import Cart from "../Cart/Cart";
+// import Menu from "../Menu/Menu";
+// export default function Navbar() {
+//   return (
+//     <div className="container d-flex justify-content-between align-items-center fixed-top">
+//       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//         <div className="navbar-nav mr-auto mt-2 mt-lg-0">
+//           <Menu />
+//           {/* <SearchForm /> */}
+//         </div>
+
+//         <Link className="nav-link" href="/">
+//           <Logo />
+//         </Link>
+
+//         <div>
+//           <div>
+//             <Login />
+//             <Cart />
+//           </div>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// }
