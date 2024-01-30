5712012951d53db82629acad6ea33c68528b8a3a
Therese <T.Otterhall@gmail.com>
2024-01-30 20:12:02 +0100

    :lipstick: thecarousel works

--- client/my-app/src/app/components/HomePage/Carousel/Carousel.jsx
+++ client/my-app/src/app/components/HomePage/Carousel/Carousel.jsx
@@ -1,59 +1,60 @@
-"use Client";
-//Måste kolla hur jag gör med bilder i next.js...att importera bild fungerar...
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import Golden from "../../../../../public/assets/carousel/golden_dog.jpg";
-// import Dalmatines from "../../../../public/assets/carousel/dalmatine_dogs.jpg";
-// import Aussie from "../../../../public/assets/carousel/aussie_dog.jpg";
-
+import Golden from "../../../../../public/assets/carousel/golden_dog.jpg";
+import Dalmatines from "../../../../../public/assets/carousel/dalmatine_dogs.jpg";
+import Aussie from "../../../../../public/assets/carousel/aussie_dog.jpg";
+//BYT BILDER//SE ÖVER STORLEKARNA
 export default function Carousel() {
-  // const [currentSlide, setCurrentSlide] = useState(0);
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setCurrentSlide((prevSlide) => (prevSlide === 2 ? 0 : prevSlide));
-  //   }, 3000);
-  //   return () => clearInterval(interval);
-  // }, []);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide === 2 ? 0 : prevSlide + 1));
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
-      <div>RENDERA UT KARUSELL</div>
-      {/* <div
-        id="carouselOfPawfect"
-        className="carousel slide"
-        data-ride="carousel"
-      >
+    <div className="carousel-container">
+      <div className="carousel slide" data-ride="carousel">
         <div className="carousel-inner">
           <div
-            className={`carousel-item  ${currentSlide === 0 ? "active" : ""}`}
+            className={`carousel-item ${currentSlide === 0 ? "active" : ""}`}
           >
             <Image
-              className="d-block w-auto h-100"
+              className="d-block w-100 img-fluid"
               src={Golden}
-              alt="Picture of the author"
-              sizes="100vw"
-              style={{
-                width: "300rem",
-                height: "auto",
-              }}
+              alt="Golden Dog"
+              width={1920}
+              height={1080}
             />
           </div>
           <div
-            className={`carousel-item  ${currentSlide === 1 ? "active" : ""}`}
+            className={`carousel-item ${currentSlide === 1 ? "active" : ""}`}
           >
             <Image
-              className="d-block w-vw"
+              className="d-block w-100 img-fluid"
               src={Dalmatines}
-              alt="Second slide"
+              alt="Dalmatine Dogs"
+              width={1920}
+              height={1080}
             />
           </div>
           <div
             className={`carousel-item ${currentSlide === 2 ? "active" : ""}`}
           >
-            <Image className="d-block w-100" src={Aussie} alt="Third slide" />
+            <Image
+              className="d-block w-100 img-fluid"
+              src={Aussie}
+              alt="Aussie Dog"
+              width={1920}
+              height={1080}
+            />
           </div>
-        </div> */}
-      {/* </div> */}
+        </div>
+      </div>
     </div>
   );
 }
